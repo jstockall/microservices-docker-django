7e41304166d0961a86d2c2ba61b46f04348a7099
jstockall <jstockall@embotics.com>
2019-02-20 11:54:42 -0800

    disable pep8

--- project/settings.py
+++ project/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
 ]
 
 JENKINS_TASKS = (
-    'django_jenkins.tasks.run_pep8',
     'django_jenkins.tasks.run_pyflakes',
     'django_jenkins.tasks.run_jslint',
     'django_jenkins.tasks.run_csslint',
